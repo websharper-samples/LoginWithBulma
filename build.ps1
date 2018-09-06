90717043f4972f69fa53f46d3288dc791093a70b
Loic Denuziere <loic.denuziere@budapest.intellifactory.com>
2018-09-06 12:32:22 +0200

    Create empty gh-pages

--- build.ps1
+++
@@ -1,9 +0,0 @@
-param ([switch] $update)
-
-if ($update) {
-  dotnet add src package WebSharper
-  dotnet add src package WebSharper.FSharp
-  dotnet add src package WebSharper.UI
-}
-
-dotnet build src
